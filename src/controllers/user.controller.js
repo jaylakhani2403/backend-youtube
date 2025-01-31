6e2ad1ad80ca5a41e8fc2ac70c37a9f45a423ffd
jaylakhani2403 <23ce062@charusat.edu.in>
2025-01-31 23:53:04 +0530

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,76 @@
 import { asynchandelar } from "../utils/asyncHendelar.js"
+import {ApiError} from "../utils/ApiError.js"
+import { User} from "../models/user.model.js"
+import {uploadCloudinary} from "../utils/cloudinary.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+
 
 const registerUser=asynchandelar(async(req,res)=>{
-    res.status(200).json({
-        message:"User Registered Successfully",
-    })
+  
+
+
+   const {fullName,email,username,password}= req.body
+   console.log("email",email);
+
+   if([
+      fullName,email,username,password].some((field)=>field?.trim()==="")){
+      throw new ApiError(400,"all fields are required")
+      
+
+   }
+  const existeduser=User.findOne({
+   $or:[{username},{email}]
+  })
+  if(existeduser){
+   throw new ApiError("already exists username or email");
+   
+  }
+
+ const avatarLocalPath = req.files?.avatar[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
+  if(!avatarLocalPath){
+   throw new ApiError(400,"avtar is required");
+   
+  }
+
+  const avatar=await uploadCloudinary(avatarLocalPath)
+  const coverImage=await uploadCloudinary(coverImageLocalPath)
+
+  if(!avatar){
+   throw new ApiError(400,"avtar is required");
+  }
+
+  const user= await User.create({
+   fullName,
+   avatar:avatar.url,
+   coverImage:coverImage?.url||"",
+   email,
+   password,
+    username: username.toLowerCase()
+
+  })
+
+const cretedUser=   await User.findById(user._id).select(
+   "-password -refreshToken"
+)
+
+if(!cretedUser){
+   throw new ApiError(500,"somthing went wrong register user");
+
+}
+
+return res.status(201).json(
+   new ApiResponse(200,cretedUser,"user register succesfully");
+)
+
+
+
 })
 
 export {registerUser}
