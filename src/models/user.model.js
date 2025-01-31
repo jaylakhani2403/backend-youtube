cfae21c9a6058e4da1904cd383bad3c9cb4c8dd7
jaylakhani2403 <23ce062@charusat.edu.in>
2025-01-31 15:28:15 +0530

    config ho gaya

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,10 @@
 import mongoose ,{Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+
+
+
 
 const userSchema=new Schema({
 
@@ -40,7 +46,7 @@ const userSchema=new Schema({
     ],
     password:{
         type:String,
-        required:[true,'passeord requried'],
+        required:[true,'passeword requried'],
 
         },
         refreshToken:{
@@ -59,4 +65,49 @@ const userSchema=new Schema({
 
 },{timestamps:true});
 
+
+userSchema.pre("save",async function (next) {
+    if (!this.isModified("password"))return next(); 
+    this.password=bcrypt.hash(this.password,10)
+    next()  
+})
+
+userSchema.methods.isPasswordCorrect=async function (password) {
+ return  await bcrypt.compare(password,this.password);
+    
+}
+userSchema.methods.generateAcessToken=function(){
+    jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+
+
+        },
+        
+        process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+userSchema.methods.generateRefToken=function(){
+     jwt.sign(
+        {
+            _id:this._id,
+           
+
+
+        },
+        
+        process.env.REFRESH_TOKEN_SECRET,{
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+
+    )
+
+}
 export  const User=mongoose.model("User",userSchema)
